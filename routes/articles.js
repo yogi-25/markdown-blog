c9464d080eeee400615cdb95e589066b5d9f5f55
Yogita Misal <55496505+yogi-25@users.noreply.github.com>
2021-01-27 11:29:57 +0530

    Update articles.js

--- routes/articles.js
+++ routes/articles.js
@@ -1,11 +1,12 @@
+//All routes under in this file 
 const express = require('express')
 const Article = require('./../models/article')
-const router = express.Router()
-
+const router = express.Router()  //used to create view 
+//New Blog route
 router.get('/new', (req, res) => {
   res.render('articles/new', { article: new Article() })
 })
-
+//Edit blog route
 router.get('/edit/:id', async (req, res) => {
   const article = await Article.findById(req.params.id)
   res.render('articles/edit', { article: article })
