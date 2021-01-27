5b8fdcf09173eebff1765bd2b2227c6792aad3ad
Yogita Misal <55496505+yogi-25@users.noreply.github.com>
2021-01-27 14:39:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require('express')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') //using this library we can actually connect to the database.
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
-
+//connection method of database is used below
+//blog is name of database
+//useNewUrlParser,useUnifiedTopology and useCreateIndex these parameters are used depends on mongodb version.
 mongoose.connect('mongodb://localhost/blog', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
 
-app.set('view engine', 'ejs')   //I had written all views in ejs .View engine convert this ejs code into HTML.
+app.set('view engine', 'ejs')   // written all views in ejs and View engine convert this ejs code into HTML.
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
-  const articles = await Article.find().sort({ createdAt: 'desc' })
-  res.render('articles/index', { articles: articles })
+  const articles = await Article.find().sort({ createdAt: 'desc' }) //sorted these article depends on when they are created .
+  res.render('articles/index', { articles: articles })  // Newer ones are at top beacuse of desc i.e desending order.
 })
 
 app.use('/articles', articleRouter)
